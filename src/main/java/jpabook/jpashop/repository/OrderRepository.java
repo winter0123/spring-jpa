dfa44b81862d48ddf54c0a5c2d2a6eb3f0b294fe
winter0123 <cloudmist.j@gmail.com>
2024-07-12 16:17:01 +0900

    order service test

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -5,6 +5,8 @@ import jpabook.jpashop.domain.Order;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 @RequiredArgsConstructor
 public class OrderRepository {
--- src/test/java/jpabook/jpashop/service/OrderServiceTest.java
+++ src/test/java/jpabook/jpashop/service/OrderServiceTest.java
@@ -6,6 +6,8 @@ import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.domain.item.Book;
+import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.exception.NotEnoughStockDataException;
 import jpabook.jpashop.repository.OrderRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -14,6 +16,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.transaction.annotation.Transactional;
 
+import static org.assertj.core.api.Fail.fail;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.springframework.test.util.AssertionErrors.assertEquals;
 
 @ExtendWith(SpringExtension.class)
@@ -28,16 +32,8 @@ class OrderServiceTest {
     @Test
     public void 상품주문() throws Exception {
         //given
-        Member member = new Member();
-        member.setName("회원1");
-        member.setAddress(new Address("서울", "중로", "123-456"));
-        em.persist(member);
-
-        Book book = new Book();
-        book.setName("시골 jpa");
-        book.setPrice(10000);
-        book.setStockQuantity(10);
-        em.persist(book);
+        Member member = createMember();
+        Book book = createBook("시골 jpa",10000,10);
 
         //when
         int orderCount = 2;
@@ -51,22 +47,63 @@ class OrderServiceTest {
         assertEquals("주문 가격은 가격*수량 이다", 10000 * orderCount, getOrder.getTotalPrice());
         assertEquals("주문 수량만큼 재고가 줄어야 한다", 8, book.getStockQuantity());
     }
-    
+
+    @Test//(expected = NotEnoughStockDataException.class) junit4 문법임
+    public void 상품주문_재고수량초과() throws Exception {
+        //given
+
+        Member member = createMember();
+        Item item = createBook("시골 jpa",10000,10);
+
+        int orderCount = 11;
+
+        //when
+        //orderService.order(member.getId(), item.getId(), orderCount);
+
+        NotEnoughStockDataException thrown = assertThrows(NotEnoughStockDataException.class, () -> {
+            orderService.order(member.getId(), item.getId(), orderCount);
+        });
+
+        //then
+        //fail("재고수량 부족으로 예외가 발생해야 한다.");
+    }
+
     @Test
     public void 주문취소() throws Exception {
         //given
-        
+        Member member = createMember();
+        Item item = createBook("시골 jpa",10000,10);
+
+        int orderCount = 2;
+        Long orderId = orderService.order(member.getId(), item.getId(), orderCount);
+
         //when
+        orderService.cancelOrder(orderId);
         
         //then
+        Order getOrder = orderRepository.findOne(orderId);
+
+        assertEquals("주문 취소소시 상태는 CANCEL", OrderStatus.CANCEL, getOrder.getStatus());
+        assertEquals("주문 취소된 상품은 그만큼 재고가 증가해야 한다", 10, item.getStockQuantity());
     }
-    
-    @Test
-    public void 상품주문_재고수량초과() throws Exception {
-        //given
-        
-        //when
-        
-        //then
+
+
+
+    private Book createBook(String name, int price, int stockQuantity) {
+        Book book = new Book();
+        book.setName(name);
+        book.setPrice(price);
+        book.setStockQuantity(stockQuantity);
+        em.persist(book);
+        return book;
     }
+
+    private Member createMember() {
+        Member member = new Member();
+        member.setName("회원1");
+        member.setAddress(new Address("서울", "중로", "123-456"));
+        em.persist(member);
+        return member;
+    }
+
 }
